58d139ee9633cc7a34ee0174e2bf93c50e0683a2
AbuHasnth <abuhsnatjiku@gmail.com>
2020-10-21 11:31:01 +0600

    first commit

---
+++ true-false.js
@@ -0,0 +1,18 @@
+const age = 0;
+if(age){
+    console.log(" condition is true");
+}
+else{
+    console.log("condition is false");
+}
+
+const name = "";
+
+// jodi null value thake taile condition always false
+
+if(name.length){
+    console.log( "condition is true")
+}
+else {
+    console.log("condition is false");
+}
