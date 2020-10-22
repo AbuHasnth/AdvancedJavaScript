d6cf38d8846b4d04cf54f3e41d34021c74e296d3
AbuHasnth <abuhsnatjiku@gmail.com>
2020-10-22 18:13:00 +0600

    onek kisu sikhlam

--- object-property.js
+++ object-property.js
@@ -1,6 +1,8 @@
 const student = [
     { name: "manna", id: 39},
     { name : "omor", id : 39},
+    { name : "omor", id : 9},
+    { name : "omor", id : 3},
 ]
 
 // console.log(student)
@@ -13,4 +15,14 @@ const output = [];
         
     }
     console.log(output)
+
+    const result = student.map ( x => x.name);
+    console.log(result)
+
+    const bigger = student.filter( x => x.id > 3);
+    console.log(bigger)
+
+    const bigger1 = student.filter( x => x.name.id>3);
+    console.log(bigger1)
+
 
