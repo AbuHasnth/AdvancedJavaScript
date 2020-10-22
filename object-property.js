114da2ca93f97f0555d7a0402fa24ea90cabd203
AbuHasnth <abuhsnatjiku@gmail.com>
2020-10-22 17:30:38 +0600

    done

---
+++ object-property.js
@@ -0,0 +1,16 @@
+const student = [
+    { name: "manna", id: 39},
+    { name : "omor", id : 39},
+]
+
+// console.log(student)
+const output = [];
+
+    for ( let i = 0; i< student.length; i++){
+        const Element= student[i].name;
+        output .push(Element);
+        
+        
+    }
+    console.log(output)
+
